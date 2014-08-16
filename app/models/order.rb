743b7e98c60c83039a456a5209c4bdeed0ea1f3a
Cullen Jett <cullenjett@gmail.com>
2014-08-16 12:21:37 -0600

    Iteration 4

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ActiveRecord::Base
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :products
+
+  def total
+    subtotals = []
+    order_items.each do |item|
+      subtotals << item.subtotal
+    end
+    subtotals.inject(&:+)
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,8 @@ class OrderItem < ActiveRecord::Base
   belongs_to :product
 
   validates :order_id, :product_id, presence: true
+
+  def subtotal
+    self.quantity * product.price
+  end
 end
